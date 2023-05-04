6e4c849101841ca70d6fda2951f65faa42955106
yostrejo <mtrejo0@ucol.mx>
2023-05-03 23:44:33 -0600

    cambios

--- src/controllers/language.controllers.js
+++ src/controllers/language.controllers.js
@@ -1,71 +1,204 @@
-import { getConnection } from "./../database/database";
-import bodyparser from "body-parser";   
+import { connection, getConnection } from "./../database/database";
+import bodyparser from "body-parser";
 import { response } from "express";
+import * as xlsx from "xlsx";
+
+const path = require('path')
+const fs = require('fs')
+const readXlsxFile = require('read-excel-file/node');
+const XLSX = require('xlsx');
 
 
 const getLanguages = async (req, res) => {
-   const connection= await getConnection();
-    // res.json("gngbvcssf");
-    const result=await connection.query("SELECT * FROM general");
-    console.log(result);
-    res.json(result);
+  const connection = await getConnection();
+  // res.json("gngbvcssf");
+  const result = await connection.query("SELECT Id_categoria, RPU, Dependencia, Men_Bim, TFA FROM general ORDER BY Id_categoria ASC");
+  console.log(result);
+  res.json(result);
 };
 
 const getLanguage = async (req, res) => {
-   const connection= await getConnection();
-    // res.json("gngbvcssf");
-     const result=await connection.query("SELECT * FROM general WHERE RPU= ?", [req.params.RPU]);
-     console.log(result);
-     res.json(result[0]);
-    
- };
+  const connection = await getConnection();
+  // res.json("gngbvcssf");
+  const result = await connection.query("SELECT * FROM general WHERE RPU= ? ", [req.params.RPU]);
+  console.log(result);
+  res.json(result[0]);
 
-
- const createLanguage = async (req, res) => {
-    const {RPU, Dependencia, Men_Bim, TFA } = req.body;
-    const connection= await getConnection();
-    const result=await connection.query('INSERT INTO general (Id_categoria, RPU, Dependencia, Men_Bim, TFA) VALUES (?, ?, ?, ?)', [RPU, DEPENDENCIA, MEN_BIM, TFA]);
-      console.log(result);
-      res.json(result);
-     
-  };
- 
-
-  const updateLanguage = async (req, res) => {
-    const connection= await getConnection();
-     
-      const result=await connection.query("UPDATE general SET ? WHERE RPU = ?", [req.body, req.params.RPU]);
-      console.log(result);
-      res.json(result);
-     
-  };
-
-  const deleteLanguage = async (req, res) => {
-    const connection= await getConnection();
-     // res.json("gngbvcssf");
-      const result=await connection.query("DELETE FROM general WHERE RPU= ?", [req.params.RPU]);
-      console.log(result);
-      res.json(result);
-     
-  };
-
- const getSector= async (req, res) => {
-    const connection= await getConnection();
-     // res.json("gngbvcssf");
-     const result=await connection.query("SELECT * FROM categorias");
-     console.log(result);
-     res.json(result);
- };
- 
- const getSector1= async (req, res) => {
-  const connection= await getConnection();
-   const result=await connection.query("SELECT * FROM `poder ejecutivo`");
-   console.log(result);
-   res.json(result);
 };
+
+
+const createLanguage = async (req, res) => {
+
+  const { Id_Categoria, RPU, DEPENDENCIA, MEN_BIM, TFA, Consumo, Importe } = req.body;
+  const connection = await getConnection();
+  const result = await connection.query('INSERT INTO general (Id_categoria, RPU, Dependencia, Men_Bim, TFA, Consumo, Importe) VALUES (?, ?, ?, ?, ?, "", "")', [Id_Categoria, RPU, DEPENDENCIA, MEN_BIM, TFA]);
+  console.log(result);
+  res.json(result);
+
+};
+
+
+const updateLanguage = async (req, res) => {
+
+  const connection = await getConnection();
+  const result = await connection.query("UPDATE general SET ? WHERE RPU = ?", [req.body, req.params.RPU]);
+  console.log(result);
+  res.json(result);
+
+};
+
+const deleteLanguage = async (req, res) => {
+
+  const connection = await getConnection();
+  // res.json("gngbvcssf");
+  const result = await connection.query("DELETE FROM general WHERE RPU= ?", [req.params.RPU]);
+  console.log(result);
+  res.json(result);
+
+};
+
+const getSector = async (req, res) => {
+
+  const connection = await getConnection();
+  // res.json("gngbvcssf");
+  const result = await connection.query("SELECT * FROM categorias");
+  console.log(result);
+  res.json(result);
+
+};
+
+const getSector1 = async (req, res) => {
+
+  const connection = await getConnection();
+  const result = await connection.query("SELECT * FROM `poder ejecutivo`");
+  console.log(result);
+  res.json(result);
+
+};
+
+
+const createRegistro = (req, res) => {
+
+  try{
+
+  const workbook = xlsx.readFile(path.join(__dirname, '../files/' + req.file.filename));
+  const sheet_name_list = workbook.SheetNames;
+  const data = xlsx.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
+
+  const CONSUMO = data.CONSUMO
+  const IMPORTE = data.IMPORTETOTAL
+  const RPU = data.RPU
+  
+  data.forEach(async (row) => {
+
+    const {RPU, CONSUMO, IMPORTETOTAL} = row;
+    const sql = `UPDATE general SET Consumo =?, Importe =?, WHERE RPU= ?`;
+    const values = [CONSUMO, IMPORTETOTAL, RPU];
+  
+  const connection = await getConnection();
+   connection.query(sql, values, (error, results) => {
+   if (error) throw error;
+   // if(error) return res.status(500).send('server error');
+         console.log(values, results);
+   
+    });
+  
+});
+} catch(error){
+  const result = {
+    status: "fail",
+    filename: req.file.originalname,
+    message: "Upload Error! message = " + error.message
+}
+res.json(result);
+}
+
+
+};
+
+//const file = fs.readFileSync(path.join(__dirname, '../files/' + req.file.filename))
+//const data = await file.arrayBuffer(file);
+//const excelfile = xlsx.read(data);
+//const excelSheet = excelfile.Sheets[excelfile.SheetNames[0]];
+//const exceljson = xlsx.utils.sheet_to_json(excelSheet);
+
+//const RPU = xlsx.utils.decode_col('0');
+//const CONSUMO = xlsx.utils.decode_col('8');
+//const IMPORTE = xlsx.utils.decode_col('16');
+
+
+
+/* Insertar los valores en la tabla de MySQL
+const query = `UPDATE general SET Consumo = '${CONSUMO}', Importe = '${IMPORTE}' WHERE RPU = '${RPU}'`;
+connection.query(query, (error, results, fields) => {
+  if (error) throw error;
+  console.log(exceljson);
+});
+}*/
+/*
+const connection= await getConnection();
+const result=await connection.query(`UPDATE general SET Consumo = '${CONSUMO}', Importe = '${IMPORTETOTAL}' WHERE RPU = '${RPU}'`, [req.files.file]);
+  console.log(result);
+  res.json(result);
+ 
+*/
+
+
+const uploadRegistro = async (req, res) => {
+  const connection = await getConnection();
+
+  const data = (__dirname, '../files' + req.file.filename)
+
+  readXlsxFile(data).then(rows => {
+
+    console.log(rows);
+    const regs = [];
+    let length = rows.length;
+
+    for (let i = 0; i < length; i++) {
+      let reg = {
+        RPU: rows[i][0],
+        CONSUMO: rows[i][8],
+        IMPORTETOTAL: rows[i][16],
+      }
+
+      regs.push(reg)
+    }
+
+
+
+
+  })
+
+  const result = await connection.query('INSERT INTO image set ?', [{ type, name, data }], (err, rows) => {
+    if (err) return res.status(500).send('server error')
+/*("UPDATE general SET ? WHERE RPU = ?", [req.body, req.params.RPU])*/;
+    console.log(result);
+    res.json(result);
+
+  })
+};
+
+const formatRegistros = async (req, res) => {
+  const connection = await getConnection();
+   const result = await connection.query("UPDATE general SET ? Consumo = '', Importe = ''");
+  console.log(result);
+  res.json(result);
+
+};
+
+const getRegistros = async (req, res) => {
+  const connection = await getConnection();
+  // res.json("gngbvcssf");
+  const result = await connection.query("SELECT * FROM general ORDER BY Id_categoria ASC");
+  console.log(result);
+  res.json(result);
+};
+
+
 
 
 
 export const methods = {
-    /* uploadExcel,*/ getSector1, getSector, getLanguages, getLanguage, createLanguage, updateLanguage, deleteLanguage
+    /* uploadExcel,*/ uploadRegistro, formatRegistros, getRegistros, createRegistro, getSector1, getSector, getLanguages, getLanguage, createLanguage, updateLanguage, deleteLanguage
 };
--- src/routes/language.routes.js
+++ src/routes/language.routes.js
@@ -1,3 +1,4 @@
+
 import { request, response, Router } from "express";
 import { methods as languageController } from "./../controllers/language.controllers";
 import { connection, getConnection } from "../database/database.js";
@@ -6,20 +7,24 @@ const path = require('path')
 const multer = require('multer')
 const fs = require('fs')
 const router = Router();
+const xlsx = require('xlsx');
+
 
 const diskstorage = multer.diskStorage({
-    destination: path.join(__dirname, '../files'),
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + 'cobcen' + file.originalname)
-    }
+  destination: path.join(__dirname, '../files'),
+  filename: (req, file, cb) => {
+    cb(null, file.originalname)
+  }
 })
 
+
 const fileUpload = multer({
-    storage: diskstorage,
-}).single('excel')
+  storage: diskstorage,
+}).single('file')
 
 
 router.get("/", languageController.getLanguages);
+router.get("/registros", languageController.getRegistros);
 router.get("/sectores", languageController.getSector);
 router.get("/poderejecutivo", languageController.getSector1);
 router.get("/:RPU", languageController.getLanguage);
@@ -27,25 +32,125 @@ router.post("/", languageController.createLanguage);
 router.put("/:RPU", languageController.updateLanguage);
 router.delete("/:RPU", languageController.deleteLanguage);
 
-router.post("/files/post", fileUpload, (req, res) => {
-   
-  req.g((err, conn) => {
-        if(err) return res.status(500).send('server error')
+router.put("/formatear", languageController.formatRegistros);
 
-        const type = req.file.mimetype
-        const name  = req.file.originalname
-        const data = fs.readFileSync(path.join(__dirname, '../files' + req.file.filename))
 
-     conn.query('INSERT INTO prueba set ?', [{type, name, data}], (err, rows) => {
-            if(err) return res.status(500).send('server error')
+router.post('/files', fileUpload, languageController.createRegistro);
+/*
+async (req, res) => {
 
-            res.send('image saved')
+  const workbook = xlsx.readFile(path.join(__dirname, '../files/' + req.file.filename));
+  const sheet_name_list = workbook.SheetNames;
+  const data = xlsx.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
+
+  //const RPU = data.map(row => row.RPU);
+  //const CONSUMO = data.map(row => row.CONSUMO);
+  //const IMPORTE = data.map(row => row.IMPORTETOTAL);
+  const connection = await getConnection();
+  //console.log(data);
+  const sql = `UPDATE general SET Consumo = ?, Importe = ? WHERE RPU = ?`;
+
+  data.forEach(row => {
+    const values = [row.CONSUMO, row.IMPORTETOTAL, row.RPU];
     
-       })
-    })
+    connection.query(sql, values, (error, results) => {
+       console.log(values, data, results);
+    }); 
+  });
+*/
+  /*const connection = await getConnection();
+  const result = await connection.query(`UPDATE general SET Consumo = ?, Importe = ? WHERE RPU = ?`);
+  console.log(result);
+  res.json(result);*/
 
-    console.log(req.file)
+
+
+//})
+
+/* router.post('/files', (req, res) => {
+ 
+
+ const workbook = xlsx.read(req.body, { type: 'buffer' });
+ const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+ const data = xlsx.utils.sheet_to_json(worksheet);
+
+ data.forEach((row) => {
+   const { RPU, CONSUMO, IMPORTETOTAL } = row;
+
+   const sql = `UPDATE general SET Consumo = ?, Importe = ? WHERE RPU = ?`;
+   const values = [CONSUMO, IMPORTETOTAL, RPU];
+
+   connection.query(sql, values, (error, results) => {
+     if (error) throw error;
+     console.log(results.affectedRows);
+   });
+  res.json(sql);
+ });
+
+ 
+}); */
+/* router.post('/files', (req, res) => {
+   const workbook = xlsx.read(req.body.archivo, { type: 'buffer' });
+   const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+   const datos = xlsx.utils.sheet_to_json(worksheet);
+   const RPU = datos.map((dato) => dato.RPU);
+   const Consumo = datos.map((dato) => dato.CONSUMO);
+   const Importe = datos.map((dato) => dato.IMPORTETOTAL);
+   const sql = "UPDATE general SET Consumo = ?, Importe = ? WHERE RPU = ?";
+   const values = datos.map((dato) => [dato.CONSUMO, dato.IMPORTETOTAL, dato.RPU]);
+   connection.query(sql, [values], (error, result) => {
+     if (error) {
+       res.status(500).send(error);
+     } else {
+       res.send('Datos guardados correctamente');
+     }
+   });
+ });*/
+
+
+
+
+
+
+/* (req, res) => {
+    const file = req.file;
+    const workbook = xlsx.read(req.file.buffer, { type: 'buffer' });
+    // obtiene la hoja de trabajo
+    const sheet = workbook.Sheets[workbook.SheetNames[0]];   
+    // convierte la hoja de trabajo en un objeto de JavaScript
+    const data = xlsx.utils.sheet_to_json(sheet);    
+    // itera sobre los datos y los inserta en la base de datos
+    data.forEach((row) => {
+      const { RPU, CONSUMO, IMPORTETOTAL } = row;     
+      // realiza una consulta para actualizar los datos en la tabla 'usuarios' que coincidan con el id del archivo de Excel
+      const query = `UPDATE general SET Consumo='${CONSUMO}', Importe='${IMPORTETOTAL}' WHERE RPU=${RPU}`;
+      connection.query(query, (error, results, fields) => {
+        if (error) throw error;
+        console.log(results);
+      });
+    });
+   res.send('Archivo cargado exitosamente');
+})*/
+/*(req, res) => {
+ 
+ 
+ 
+req.getConnection((err, connection) => {
+    if(err) return res.status(500).send('server error')
+
+        const data = fs.readFileSync(path.join(__dirname, '../files' + req.file.filename)),
+       
+ 
+connection.query('INSERT INTO image set ?', [{type, name, data}], (err, rows) => {
+            if(err) return res.status(500).send('server error')
+ 
+        res.send('image saved')
+ 
+   })
 })
+
+console.log(req.file)
+})*/
 
 
 export default router;
