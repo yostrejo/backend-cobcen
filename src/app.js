43516adb21858023ff97dfb058d5788dd54fb075
yostrejo <mtrejo0@ucol.mx>
2023-06-02 03:34:08 -0600

    cambios

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const reader= require('xlsx');
 const loginRoutes = require('../src/routes/login')
 
 //settings
-app.set("port", 3030);
+app.set("port", process.env.PORT | PORT);
 
 //midlewares
 app.use(morgan("dev"));
@@ -26,29 +26,7 @@ app.use(cors());
 //Routes
 app.use("/", loginRoutes);
 app.use("/api/languages", languageRoutes);
-//app.use("api/document", fileUpload);
-/* app.get("/readeexcelfile",(req, res) => {
- let fileName = req.query.fileName;
- let data = []
- try {
-  const file = reader.readFile('publicfiles/' + fileName + ".xlsx");
-  const sheetNames = file.SheetNames
 
-  for (let i = 0; i < sheetNames.length; i++) {
-    const arr = reader.utils.sheet_to_json(
-      file.Sheets[sheetNames[i]])
-      arr.forEach((res) => {
-        data.push(res)
-      })
-  }
-  res.send(data);
-  } catch (err) {
-    res.send(err);
-  }
- })*/
-//app.post('/transactions',(req, res) => {
-  //  console.log('Me esta llegando algoooo')
-//})
 
 
 export default app;
